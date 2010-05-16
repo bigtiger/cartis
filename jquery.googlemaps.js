85617596c2bdfa4a6ffcf3613fbee7f94a1c6102
Jim Remsik <bigtiger@hashrocket.com>
2010-05-15 21:08:00 -0400

    Cleanup;
    * Whitespace normalization
    * Add braces to all if statements

--- jquery.googlemaps.js
+++ jquery.googlemaps.js
@@ -42,15 +42,13 @@
         geocoder.getLatLng(opts.geocode, function(center) {
           if (!center) {
             alert(address + " not found");
-          }
-          else {
-        $.googleMaps.gMap.setCenter(center, opts.depth);
+          } else {
+            $.googleMaps.gMap.setCenter(center, opts.depth);
             $.googleMaps.latitude = center.x;
             $.googleMaps.longitude = center.y;
           }
-      });
-      }
-      else {
+        });
+      } else {
         // Latitude & Longitude Center Point
         var center	= $.googleMaps.mapLatLong(opts.latitude, opts.longitude);
         // Set the center of the Map with the new Center Point and Depth
@@ -58,9 +56,10 @@
       }
 
       // POLYLINE
-      if ( opts.polyline )
+      if ( opts.polyline ) {
         // Draw a PolyLine on the Map
         $.googleMaps.gMap.addOverlay($.googleMaps.mapPolyLine(opts.polyline));
+      }
       // GEODESIC
       if ( opts.geodesic ) {
         $.googleMaps.mapGeoDesic(opts.geodesic);
@@ -76,38 +75,45 @@
       }
 
       // LAYER
-      if ( opts.layer )
+      if ( opts.layer ) {
         // Set the Custom Layer
         $.googleMaps.gMap.addOverlay(new GLayer(opts.layer));
+      }
 
       // MARKERS
-      if ( opts.markers )
+      if ( opts.markers ) {
         $.googleMaps.mapMarkers(opts.markers);
+      }
 
       // CONTROLS
       if ( opts.controls.type || opts.controls.zoom ||  opts.controls.mapType ) {
         $.googleMaps.mapControls(opts.controls);
-      }
-      else {
-        if ( !opts.controls.hide )
+      } else {
+        if ( !opts.controls.hide ) {
           $.googleMaps.gMap.setUIToDefault();
+        }
       }
 
       // SCROLL
-      if ( opts.scroll )
+      if ( opts.scroll ) {
         $.googleMaps.gMap.enableScrollWheelZoom();
-      else if ( !opts.scroll )
-        $.googleMaps.gMap.disableScrollWheelZoom();
+      } else {
+        if ( !opts.scroll ) {
+          $.googleMaps.gMap.disableScrollWheelZoom();
+        }
+      }
 
       // LOCAL SEARCH
-      if ( opts.controls.localSearch )
+      if ( opts.controls.localSearch ) {
         $.googleMaps.gMap.enableGoogleBar();
-      else
+      } else {
         $.googleMaps.gMap.disableGoogleBar();
+      }
 
       // FEED (RSS/KML)
-      if ( opts.feed )
+      if ( opts.feed ) {
         $.googleMaps.gMap.addOverlay(new GGeoXml(opts.feed));
+      }
 
       // TRAFFIC INFO
       if ( opts.trafficInfo ) {
@@ -127,6 +133,7 @@
       $.googleMaps.gMap.addOverlay(svOverlay);
       }
     },
+
     mapGeoDesic: function(options) {
       // Default GeoDesic Options
       geoDesicDefaults = {
@@ -148,16 +155,20 @@
       );
       $.googleMaps.gMap.addOverlay(polyline);
     },
+
     localSearchControl: function(options) {
       var controlLocation = $.googleMaps.mapControlsLocation(options.location);
       $.googleMaps.gMap.addControl(new $.googleMaps.gMap.LocalSearch(), new GControlPosition(controlLocation, new GSize(options.x,options.y)));
     },
+
     getLatitude: function() {
       return $.googleMaps.latitude;
     },
+
     getLongitude: function() {
       return $.googleMaps.longitude;
     },
+
     directions: {},
     latitude: '',
     longitude: '',
@@ -166,19 +177,20 @@
     marker: {},
     gMap: {},
     defaults: {
-    // Default Map Options
-      latitude:	37.4419,
-      longitude:	-122.1419,
-      depth:		13,
-      scroll:		true,
+      // Default Map Options
+      latitude: 37.4419,
+      longitude: -122.1419,
+      depth: 13,
+      scroll: true,
       trafficInfo: false,
       streetViewOverlay: false,
       controls: {
         hide: false,
         localSearch: false
       },
-      layer:		null
+      layer: null
     },
+
     mapPolyLine: function(options) {
       // Default PolyLine Options
       polylineDefaults = {
@@ -199,10 +211,12 @@
         options.pixels
       );
     },
+
     mapLatLong: function(latitude, longitude) {
       // Returns Latitude & Longitude Center Point
       return new GLatLng(latitude, longitude);
     },
+
     mapPanOptions: function(options) {
       // Returns Panning Options
       var panDefaults = {
@@ -212,53 +226,69 @@
       }
       return options = $.extend({}, panDefaults, options);
     },
+
     mapMarkersOptions: function(icon) {
       //Define an icon
       var gIcon = new GIcon(G_DEFAULT_ICON);
-      if ( icon.image )
+      if ( icon.image ) {
         // Define Icons Image
         gIcon.image = icon.image;
-      if ( icon.shadow )
+      }
+      if ( icon.shadow ) {
         // Define Icons Shadow
         gIcon.shadow = icon.shadow;
-      if ( icon.iconSize )
+      }
+      if ( icon.iconSize ) {
         // Define Icons Size
         gIcon.iconSize = new GSize(icon.iconSize);
-      if ( icon.shadowSize )
+      }
+      if ( icon.shadowSize ) {
         // Define Icons Shadow Size
         gIcon.shadowSize = new GSize(icon.shadowSize);
-      if ( icon.iconAnchor )
+      }
+      if ( icon.iconAnchor ) {
         // Define Icons Anchor
         gIcon.iconAnchor = new GPoint(icon.iconAnchor);
-      if ( icon.infoWindowAnchor )
+      }
+      if ( icon.infoWindowAnchor ) {
         // Define Icons Info Window Anchor
         gIcon.infoWindowAnchor = new GPoint(icon.infoWindowAnchor);
-      if ( icon.dragCrossImage )
+      }
+      if ( icon.dragCrossImage ) {
         // Define Drag Cross Icon Image
         gIcon.dragCrossImage = icon.dragCrossImage;
-      if ( icon.dragCrossSize )
+      }
+      if ( icon.dragCrossSize ) {
         // Define Drag Cross Icon Size
         gIcon.dragCrossSize = new GSize(icon.dragCrossSize);
-      if ( icon.dragCrossAnchor )
+      }
+      if ( icon.dragCrossAnchor ) {
         // Define Drag Cross Icon Anchor
         gIcon.dragCrossAnchor = new GPoint(icon.dragCrossAnchor);
-      if ( icon.maxHeight )
+      }
+      if ( icon.maxHeight ) {
         // Define Icons Max Height
         gIcon.maxHeight = icon.maxHeight;
-      if ( icon.PrintImage )
+      }
+      if ( icon.PrintImage ) {
         // Define Print Image
         gIcon.PrintImage = icon.PrintImage;
-      if ( icon.mozPrintImage )
+      }
+      if ( icon.mozPrintImage ) {
         // Define Moz Print Image
         gIcon.mozPrintImage = icon.mozPrintImage;
-      if ( icon.PrintShadow )
+      }
+      if ( icon.PrintShadow ) {
         // Define Print Shadow
         gIcon.PrintShadow = icon.PrintShadow;
-      if ( icon.transparent )
+      }
+      if ( icon.transparent ) {
         // Define Transparent
         gIcon.transparent = icon.transparent;
+      }
       return gIcon;
     },
+
     mapMarkers: function(markers) {
       if ( typeof(markers.length) == 'undefined' ) {
         // One marker only. Parse it into an array for consistency.
@@ -277,33 +307,37 @@
         if (marker.geocode) {
           var geocoder = new GClientGeocoder();
           geocoder.getLatLng(marker.geocode, function(center) {										
-            if (!center) 
+            if (!center) {
               alert(address + " not found");
-            else 
+            } else {
               gmarker = new GMarker(center, {draggable: marker.draggable, icon: gIcon});
+            }
           });
-        }
-        else if ( marker.latitude && marker.longitude ) {
-          // Latitude & Longitude Center Point
-          center = $.googleMaps.mapLatLong(marker.latitude, marker.longitude);
-          gmarker = new GMarker(center, {draggable: marker.draggable, icon: gIcon});
+        } else {
+          if ( marker.latitude && marker.longitude ) {
+            // Latitude & Longitude Center Point
+            center = $.googleMaps.mapLatLong(marker.latitude, marker.longitude);
+            gmarker = new GMarker(center, {draggable: marker.draggable, icon: gIcon});
+          }
         }
         $.googleMaps.gMap.addOverlay(gmarker);
         if ( marker.info ) {
           // Hide Div Layer With Info Window HTML
           $(marker.info.layer).hide();
           // Marker Div Layer Exists
-          if ( marker.info.popup )
+          if ( marker.info.popup ) {
             // Map Marker Shows an Info Box on Load
             gmarker.openInfoWindowHtml($(marker.info.layer).html());
-          else
+          } else {
             gmarker.bindInfoWindowHtml( $(marker.info.layer).html().toString() );
+          }
         }
 
         gmarkers.push(gmarker);
       });
       return gmarkers;
     },
+
     mapControlsLocation: function(location) {
       switch (location) {
         case 'G_ANCHOR_TOP_RIGHT' :
@@ -321,6 +355,7 @@
       }
       return;
     },
+
     mapControl: function(control) {
       switch (control) {
         case 'GLargeMapControl3D' :
@@ -356,6 +391,7 @@
       }
       return;
     },
+
     mapTypeControl: function(type) {
       switch ( type ) {
         case 'G_NORMAL_MAP' :
@@ -370,6 +406,7 @@
       }
       return;
     },
+
     mapControls: function(options) {
       // Default Controls Options
       controlsDefaults = {
@@ -404,29 +441,35 @@
       if ( options.mapType ) {
         if ( options.mapType.length >= 1 ) {
           for ( i = 0; i<options.mapType.length; i++) {
-            if ( options.mapType[i].remove )
+            if ( options.mapType[i].remove ) {
               $.googleMaps.gMap.removeMapType($.googleMaps.mapTypeControl(options.mapType[i].remove));
-            if ( options.mapType[i].add )
+            }
+            if ( options.mapType[i].add ) {
               $.googleMaps.gMap.addMapType($.googleMaps.mapTypeControl(options.mapType[i].add));
+            }
           }
         }
         else {
-          if ( options.mapType.add )
+          if ( options.mapType.add ) {
             $.googleMaps.gMap.addMapType($.googleMaps.mapTypeControl(options.mapType.add));
-          if ( options.mapType.remove )
+          }
+          if ( options.mapType.remove ) {
             $.googleMaps.gMap.removeMapType($.googleMaps.mapTypeControl(options.mapType.remove));
+          }
         }
       }
     },
+
     geoCode: function(options) {
       geocoder = new GClientGeocoder();
 
       geocoder.getLatLng(options.address, function(point) {
-        if (!point)
+        if (!point) {
           alert(address + " not found");
-        else
-        $.googleMaps.gMap.setCenter(point, options.depth);
-    });
+        } else {
+          $.googleMaps.gMap.setCenter(point, options.depth);
+        }
+      });
     }
   };
 })(jQuery);
